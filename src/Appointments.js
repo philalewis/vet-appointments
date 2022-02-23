cf8e8e0dc9d805a8c833189c9178e6b494b5ec66
Phil Lewis <phillewis1985@gmail.com>
2022-02-23 10:50:28 -0700

    Build functionality for form and display

--- src/Appointments.js
+++ src/Appointments.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Appointments = () => {
+  const [ appointments, setAppointments ] = useState([])
+
+  const getAppointments = async () => {
+    const getData = await fetch('http://localhost:3001/appointments')
+    const appts = await getData.json()
+  }
+  
+  useEffect(() => {
+    getAppointments()
+  }, [])
+
+  const populateAppointments = () =>  {
+    return appointments.map(appointment => {
+      return (
+        <div key={appointment.id}>
+          <p>PET: {appointment.pet}</p>
+          <p>DATE: {appointment.date}</p>
+          <p>TIME: {appointment.time}</p>
+        </div>
+      )
+    })
+  }
+
   return (
     <section>
-
+      { populateAppointments() }
     </section>
   )
 }
--- src/Form.js
+++ src/Form.js
@@ -5,11 +5,73 @@ const Form = () => {
   const [ date, setDate ] = useState('')
   const [ time, setTime ] = useState('')
 
+  const handleChange = (event) => {
+    event.preventDefault()
+    switch(event.target.name) {
+      case 'petName':
+        setPetName(event.target.value)
+        break
+      case 'date':
+        setDate(event.target.value)
+        break
+      case 'time':
+        setTime(event.target.value)
+        break
+    }
+  }
+
+  const makeNewAppointment = async () => {
+    const appointment = await fetch('http://localhost:3001/appointments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        pet: petName,
+        date: date,
+        time: time
+      })
+    })
+  }
+
+  const clearInputs = () => {
+    setPetName('')
+    setDate('')
+    setTime('')
+  }
+
+  const handleClick = event => {
+    event.preventDefault()
+    makeNewAppointment()
+    clearInputs()
+  }
+
   return (
     <form>
-      <input type="text" value={petName} onChange={}/>
-      <input type="text" value={date} onChange={} />
-      <input type="text" value={time} onChange={} />
+      <input 
+        type="text" 
+        placeholder="Your pet's name" 
+        name="petName" 
+        value={petName} 
+        onChange={event => handleChange(event)} 
+      />
+      <input 
+        type="text" 
+        placeholder="Date" 
+        name="date" 
+        value={date} 
+        onChange={event => handleChange(event)} 
+      />
+      <input 
+        type="text" 
+        placeholder="Time" 
+        name="time" 
+        value={time} 
+        onChange={event => handleChange(event)} 
+      />
+      <button 
+        onClick={event => handleClick(event)}
+      >SUBMIT</button>
     </form>
   )
 }
